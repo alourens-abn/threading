a9d9315f668b71a028d48331e1e9a6d015a1a00f
Albert Lourens <albert.lourens@nl.abnamro.com>
2025-03-31 16:26:31 +0200

    Added executor poc

--- src/main/java/com/workshop/threading/blocking/BlockingController.java
+++ src/main/java/com/workshop/threading/blocking/BlockingController.java
@@ -2,6 +2,11 @@ package com.workshop.threading.blocking;
 
 import com.workshop.threading.io.MockIO;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.stream.Collectors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,10 +16,30 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/blocking")
 public class BlockingController {
 
+  private final ExecutorService executorService;
+
+  public BlockingController() {
+    this.executorService = Executors.newFixedThreadPool(400);
+  }
+
   @GetMapping()
   public String blocking() {
-    MockIO.makeDbCall();
-    MockIO.makeRestCall();
+
+    Future<?> dbFuture = executorService.submit(() -> {
+      MockIO.makeDbCall();
+      return null;
+    });
+    Future<?> restFuture = executorService.submit(() -> {
+      MockIO.makeRestCall();
+      return null;
+    });
+
+    try {
+      dbFuture.get(); // Wait for the DB call to complete
+      restFuture.get(); // Wait for the REST call to complete
+    } catch (InterruptedException | ExecutionException e) {
+      e.printStackTrace();
+    }
 
     List<Thread> myThreads = Thread.getAllStackTraces()
         .keySet()
