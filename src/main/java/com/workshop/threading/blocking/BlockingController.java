d75174f7336afbea9759d90f1da7f6ceead3c59c
Albert Lourens <albert.lourens@nl.abnamro.com>
2025-04-09 16:28:28 +0200

    Added problem template

--- src/main/java/com/workshop/threading/blocking/BlockingController.java
+++ src/main/java/com/workshop/threading/blocking/BlockingController.java
@@ -1,14 +1,12 @@
 package com.workshop.threading.blocking;
 
-import com.workshop.threading.io.MockIO;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.container.AsyncResponse;
 import jakarta.ws.rs.container.Suspended;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import jdk.jshell.spi.ExecutionControl.NotImplementedException;
 
 @Path("/sandwich")
 public class BlockingController {
@@ -16,29 +14,14 @@ public class BlockingController {
   private final ExecutorService executorService;
 
   public BlockingController() {
-    this.executorService = Executors.newFixedThreadPool(10);
+    this.executorService = Executors.newFixedThreadPool(400);
   }
 
-  @GET("/cheese")
+  @GET
+  @Path("/cheese")
   public void blocking(@Suspended final AsyncResponse asyncResponse) {
-    Future<?> dbFuture = executorService.submit(() -> {
-      MockIO.makeDbCall();
-      return null;
-    });
-    Future<?> restFuture = executorService.submit(() -> {
-      MockIO.makeRestCall();
-      return null;
-    });
-
-    executorService.submit(() -> {
-      try {
-        dbFuture.get(); // Wait for the DB call to complete
-        restFuture.get(); // Wait for the REST call to complete
-        asyncResponse.resume("ok");
-      } catch (InterruptedException | ExecutionException e) {
-        asyncResponse.resume(e);
-      }
-    });
+    //todo: Add your code here
+    asyncResponse.resume(new NotImplementedException("Not implemented yet"));
   }
 
 
