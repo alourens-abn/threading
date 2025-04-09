577ec47e605bf721a91ffb856847530c8d72c758
Albert Lourens <albert.lourens@nl.abnamro.com>
2025-04-09 16:21:43 +0200

    Added problem template

--- src/main/java/com/workshop/threading/blocking/BlockingController.java
+++ src/main/java/com/workshop/threading/blocking/BlockingController.java
@@ -1,6 +1,8 @@
 package com.workshop.threading.blocking;
 
 import com.workshop.threading.io.MockIO;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +11,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/sandwich")
 public class BlockingController {
 
+  private final ExecutorService executorService;
+
+  public BlockingController() {
+    //don't change any values here
+    this.executorService = Executors.newFixedThreadPool(400);
+  }
+
   @GetMapping("/cheese")
   public String getCheeseSandwich() {
+    //todo: add your executor thread pool implementation here
     MockIO.cutCheese();
     MockIO.butterBread();
 
