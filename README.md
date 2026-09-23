81ef6903d37d2b294c573726af2a4ee3aca4ae72
priyanka <sabarishabu@gmail.com>
2026-09-23 09:16:31 +0200

    Revamp level 3 changes

--- README.md
+++ README.md
@@ -4,3 +4,19 @@ To run your gatling load tests go to you intellij terminal and run the following
 ```bash 
 mvn gatling:test
 ```
+
+### ✅ Option 1: `Servlet Async` + `Virtual Threads`
+
+## Free Tomcat thread, delegate to a virtual thread.
+![img_3.png](img_3.png)
+
+✅ Option 2: `DeferredResult` + `CompletableFuture`
+
+Clean async chaining with virtual thread support.
+![img.png](img.png)
+
+✅ Option 3: Use `HttpServer.create()` (Reactor Netty)
+![img_1.png](img_1.png)
+![img_2.png](img_2.png)
+
+✅ Option 4: Switch to WebFlux
--- src/main/java/com/workshop/threading/blocking/BlockingController.java
+++ src/main/java/com/workshop/threading/blocking/BlockingController.java
@@ -5,21 +5,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 @RestController
 @RequestMapping("/sandwich")
 public class BlockingController {
 
   //todo task 2: uncomment this code when you are told to implement task 2
-//  ExecutorService executorService = Executors.newFixedThreadPool(400);
+  ExecutorService executorService = Executors.newFixedThreadPool(400);
 
 
   @GetMapping("/cheese")
   public String getCheeseSandwich() {
-    //todo task 2: use your executor service to make these 2 methods run concurrently
-    //hint task 2: you do not need to change the MockIO class
-    MockIO.cutCheese();
-    MockIO.butterBread();
-
+    try {
+      Future<?> cheeseFuture = executorService.submit(MockIO::cutCheese);
+      Future<?> butterFuture = executorService.submit(MockIO::butterBread);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
     return "ok";
   }
 }
